5b77fc8ad055f2ef8091c7db1a7057ea92949c93
Alvaro Soto <alvaro.sg.design@gmail.com>
2020-10-02 22:32:13 -0500

    notes

--- ejercicios/Math_01.py
+++ ejercicios/Math_01.py
@@ -2,19 +2,21 @@
 # Alvaro Soto. 2020
 # Cod. Math_01.py
 
-class roman():
+class roman(object):
     pass
     def __init__(args):
         pass
     def function(args):
         pass
 
-class decimal(valor):
-    pass
-    def __init__(args):
-        pass
-    def function(args):
-        pass
+class decimal():
+    def __init__(self, data):
+        self.count = len(data)
+        #pass
+
+    # Operator in index for roman number
+    def Operator(self): # Select index for numbers
+        return self.count + 5, data[1:3]
 
 # Waiting for data
 while True:
