b025a94aada46b16aad972b6b39ae6da116653f7
Alvaro Soto <alvaro.sg.design@gmail.com>
2020-10-02 21:21:01 -0500

    Insert object

--- ejercicios/Math_01.py
+++ ejercicios/Math_01.py
@@ -9,15 +9,16 @@ class roman(object):
     def function(args):
         pass
 
-class decimal(valor):
-    pass
-    def __init__(args):
-        pass
-    def function(args):
-        pass
+class decimal():
+    def __init__(self, data):
+        self.count = len(data)
+        #pass
+    def Operator(self):
+        return self.count + 5
 
 # Waiting for data
 while True:
     data=input("Convert - decimal numerals to roman numerals / roman numerals to decimal numerals: \n")
     call = decimal(data)
-    #print(call)
+    print(call.Operator())
+    break
